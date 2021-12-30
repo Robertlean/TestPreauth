a7e02e12b8ca22ec742baad16100ac2e7d7291e4
Robert_Lean <robert_lean@live.com.ar>
2021-12-30 14:36:03 -0300

    Correction Game-01

--- game-01/app.js
+++ game-01/app.js
@@ -1,21 +1,14 @@
 const process = require('process')
 let m = process.argv[2]
 const n = process.argv[3]
-
 m = m.split(',').map(item => parseInt(item, 10))
-sumaEnteros = (val1, val2) => {
-    let sub = []
-    for (let i = 0; i < val1.length; i++) {
-        for (let j = 0; j < val1.length; j++) {
-            if ((val1[i] != val1[j])) {
-                if ((val1[i] + val1[j]) == val2) {
-                    sub.push(val1[i]);
-                    sub.push(val1[j]); 
-                    return console.log(sub)   
-                }
-            }
-        }
+subsetSum = (val1, val2) => {
+    let sub = []; x = []
+    val1.map( result => x.push(val2-result))
+    for(let i = 0; i < x.length; i++){
+        let result = val1.find(element => element == x[i])
+        if(result){sub.push(val1[i]); sub.push(result); return console.log(sub)}
     }
-    return console.log('I did not find two values ​​for the sum')
+    return console.log('I did not find two values for the sum')
 }
-sumaEnteros(m,n)
+subsetSum(m,n)
